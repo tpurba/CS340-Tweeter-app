16431e39cb817e2202c26780474d9d5077ac70bd
tpurba <tpurba@byu.edu>
2023-10-05 11:53:14 -0600

    Made GetFeedTask.java extend PageTask. Changed GetFeedTask.java key to be a ITEMS_KEY

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetFeedTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetFeedTask.java
@@ -17,57 +17,19 @@ import edu.byu.cs.tweeter.util.Pair;
 /**
  * Background task that retrieves a page of statuses from a user's feed.
  */
-public class GetFeedTask extends BackgroundTask {
+public class GetFeedTask extends PageTask<Status> {
     private static final String LOG_TAG = "GetFeedTask";
-    public static final String STATUSES_KEY = "statuses";
-    public static final String MORE_PAGES_KEY = "more-pages";
 
-    /**
-     * Auth token for logged-in user.
-     */
-    private AuthToken authToken;
-    /**
-     * The user whose feed is being retrieved.
-     * (This can be any user, not just the currently logged-in user.)
-     */
-    private User targetUser;
-    /**
-     * Maximum number of statuses to return (i.e., page size).
-     */
-    private int limit;
-    /**
-     * The last status returned in the previous page of results (can be null).
-     * This allows the new page to begin where the previous page ended.
-     */
-    List<Status> statuses;
-    boolean hasMorePages;
-    private Status lastStatus;
 
     public GetFeedTask(AuthToken authToken, User targetUser, int limit, Status lastStatus,
                        Handler messageHandler) {
-        super(messageHandler);
-        this.authToken = authToken;
-        this.targetUser = targetUser;
-        this.limit = limit;
-        this.lastStatus = lastStatus;
+        super(messageHandler, authToken, targetUser, limit, lastStatus);
     }
-
     @Override
-    protected void doTask() {
-        Pair<List<Status>, Boolean> pageOfStatus = getFeed();
-        statuses = pageOfStatus.getFirst();
-        hasMorePages = pageOfStatus.getSecond();
-    }
-
-    @Override
-    protected void loadSuccessBundle(Bundle msgBundle) {
-        msgBundle.putSerializable(STATUSES_KEY, (Serializable) statuses);
-        msgBundle.putBoolean(MORE_PAGES_KEY, hasMorePages);
-    }
-
-
-    private Pair<List<Status>, Boolean> getFeed() {
-        Pair<List<Status>, Boolean> pageOfStatus = getFakeData().getPageOfStatus(lastStatus, limit);
+    protected Pair<List<Status>, Boolean> getItems() {
+        Pair<List<Status>, Boolean> pageOfStatus = getFakeData().getPageOfStatus(getLastItem(), getLimit());
         return pageOfStatus;
     }
+
+
 }
