4b860a64a7058d8a22cad2089a9d794e98479045
tpurba <tpurba@byu.edu>
2023-10-09 21:15:00 -0600

    Changed GetFollowingHandler.java to implement HandlerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetFollowingHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetFollowingHandler.java
@@ -12,30 +12,60 @@ import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowingTask;
 import edu.byu.cs.tweeter.model.domain.User;
 
-public class GetFollowingHandler extends Handler {
+public class GetFollowingHandler extends HandlerTask<FollowService.FollowObserver>{
     private FollowService.FollowObserver observer;
 
     public GetFollowingHandler(FollowService.FollowObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(),observer);
         this.observer = observer;
     }
 
     @Override
-    public void handleMessage(@NonNull Message msg) {
-        boolean success = msg.getData().getBoolean(GetFollowingTask.SUCCESS_KEY);
-        if (success) {
-            List<User> followees = (List<User>) msg.getData().getSerializable(GetFollowingTask.ITEMS_KEY);
-            boolean hasMorePages = msg.getData().getBoolean(GetFollowingTask.MORE_PAGES_KEY);
-            observer.addMoreFollowees(followees, hasMorePages);
-        } else if (msg.getData().containsKey(GetFollowingTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(GetFollowingTask.MESSAGE_KEY);
-            observer.displayError("Failed to get following: " + message);
+    protected String getSuccessKey() {
+        return GetFollowingTask.SUCCESS_KEY;
+    }
 
+    @Override
+    protected String getMessageKey() {
+        return GetFollowingTask.MESSAGE_KEY;
+    }
 
-        } else if (msg.getData().containsKey(GetFollowingTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(GetFollowingTask.EXCEPTION_KEY);
-            observer.displayException(ex); //want to keep exceptions in presenter
+    @Override
+    protected String getExceptionKey() {
+        return GetFollowingTask.EXCEPTION_KEY;
+    }
 
-        }
+    @Override
+    protected void handleSuccess(Message msg) {
+        List<User> followees = (List<User>) msg.getData().getSerializable(GetFollowingTask.ITEMS_KEY);
+        boolean hasMorePages = msg.getData().getBoolean(GetFollowingTask.MORE_PAGES_KEY);
+        observer.addMoreFollowees(followees, hasMorePages);
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(GetFollowingTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(GetFollowingTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.displayError("Failed to get following: " + message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.displayException(exception); //want to keep exceptions in presenter
     }
 }
