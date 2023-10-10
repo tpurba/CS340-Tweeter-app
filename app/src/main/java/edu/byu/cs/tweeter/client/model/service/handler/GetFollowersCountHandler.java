2c09d416f09c7c5c9a0210f4d28076d06b687e6d
tpurba <tpurba@byu.edu>
2023-10-09 21:02:58 -0600

    Changed GetFollowersCountHandler.java to implement HandlerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetFollowersCountHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetFollowersCountHandler.java
@@ -9,26 +9,59 @@ import androidx.annotation.NonNull;
 import edu.byu.cs.tweeter.client.model.service.FollowerService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowersCountTask;
 
-public class GetFollowersCountHandler extends Handler {
+public class GetFollowersCountHandler extends HandlerTask<FollowerService.MainActivityObserver> {
     private FollowerService.MainActivityObserver observer;
 
     public GetFollowersCountHandler(FollowerService.MainActivityObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(), observer);
         this.observer = observer;
     }
 
     @Override
-    public void handleMessage(@NonNull Message msg) {
-        boolean success = msg.getData().getBoolean(GetFollowersCountTask.SUCCESS_KEY);
-        if (success) {
-            int count = msg.getData().getInt(GetFollowersCountTask.COUNT_KEY);
-            observer.getFollowerCountSuccess(count);
-        } else if (msg.getData().containsKey(GetFollowersCountTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(GetFollowersCountTask.MESSAGE_KEY);
-            observer.getFollowerCountFailed("Failed to get followers count: " + message);
-        } else if (msg.getData().containsKey(GetFollowersCountTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(GetFollowersCountTask.EXCEPTION_KEY);
-            observer.getFollowerCountFailed("Failed to get followers count because of exception: " + ex.getMessage());
-        }
+    protected String getSuccessKey() {
+        return GetFollowersCountTask.SUCCESS_KEY;
+    }
+
+    @Override
+    protected String getMessageKey() {
+        return GetFollowersCountTask.MESSAGE_KEY;
+    }
+
+    @Override
+    protected String getExceptionKey() {
+        return GetFollowersCountTask.EXCEPTION_KEY;
+    }
+
+    @Override
+    protected void handleSuccess(Message msg) {
+        int count = msg.getData().getInt(GetFollowersCountTask.COUNT_KEY);
+        observer.getFollowerCountSuccess(count);
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(GetFollowersCountTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(GetFollowersCountTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.getFollowerCountFailed("Failed to get followers count: " + message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.getFollowerCountFailed("Failed to get followers count because of exception: " + exception.getMessage());
     }
 }
