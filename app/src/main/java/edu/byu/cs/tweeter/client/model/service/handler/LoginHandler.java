89ceb5d3abaa851f86cbd771324669b61242f1ca
tpurba <tpurba@byu.edu>
2023-10-09 21:43:25 -0600

    Changed LoginHandler.java to implement HandlerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/LoginHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/LoginHandler.java
@@ -15,30 +15,63 @@ import edu.byu.cs.tweeter.model.domain.User;
 /**
  * Message handler (i.e., observer) for LoginTask
  */
-public class LoginHandler extends Handler {
+public class LoginHandler extends HandlerTask<UserService.LoginObserver> {
     private UserService.LoginObserver observer;
 
     public LoginHandler(UserService.LoginObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(),observer);
         this.observer = observer;
     }
 
     @Override
-    public void handleMessage(@NonNull Message msg) { //for both login and follow
-        boolean success = msg.getData().getBoolean(LoginTask.SUCCESS_KEY);
-        if (success) {
-            User loggedInUser = (User) msg.getData().getSerializable(LoginTask.USER_KEY);
-            AuthToken authToken = (AuthToken) msg.getData().getSerializable(LoginTask.AUTH_TOKEN_KEY);
-            //cache user session information
-            Cache.getInstance().setCurrUser(loggedInUser);
-            Cache.getInstance().setCurrUserAuthToken(authToken);
-            observer.loginSucceeded(authToken, loggedInUser);
-        } else if (msg.getData().containsKey(LoginTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(LoginTask.MESSAGE_KEY);
-            observer.loginFailed(message);
-        } else if (msg.getData().containsKey(LoginTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(LoginTask.EXCEPTION_KEY);
-            observer.loginFailed("Failed to login because of exception: " + ex.getMessage());
-        }
+    protected String getSuccessKey() {
+        return LoginTask.SUCCESS_KEY;
+    }
+
+    @Override
+    protected String getMessageKey() {
+        return LoginTask.MESSAGE_KEY;
+    }
+
+    @Override
+    protected String getExceptionKey() {
+        return LoginTask.EXCEPTION_KEY;
+    }
+
+    @Override
+    protected void handleSuccess(Message msg) {
+        User loggedInUser = (User) msg.getData().getSerializable(LoginTask.USER_KEY);
+        AuthToken authToken = (AuthToken) msg.getData().getSerializable(LoginTask.AUTH_TOKEN_KEY);
+        //cache user session information
+        Cache.getInstance().setCurrUser(loggedInUser);
+        Cache.getInstance().setCurrUserAuthToken(authToken);
+        observer.loginSucceeded(authToken, loggedInUser);
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(LoginTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(LoginTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.loginFailed(message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.loginFailed("Failed to login because of exception: " + exception.getMessage());
     }
 }
