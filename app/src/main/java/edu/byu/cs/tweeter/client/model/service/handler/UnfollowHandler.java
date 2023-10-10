9db995fff40f2dafd90f620300efa08a5af9c1d9
tpurba <tpurba@byu.edu>
2023-10-09 21:59:56 -0600

    Changed UnfollowHandler.java to implement HandlerTask.Java.

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/UnfollowHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/UnfollowHandler.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.UnfollowTask;
 
-public class UnfollowHandler extends Handler {
+public class UnfollowHandler extends HandlerTask<FollowService.MainActivityObserver> {
     private FollowService.MainActivityObserver observer;
 
     public UnfollowHandler(FollowService.MainActivityObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(), observer);
         this.observer = observer;
     }
 
@@ -31,5 +31,52 @@ public class UnfollowHandler extends Handler {
         }
 
         observer.setFollowButton(true);
+    }
+
+    @Override
+    protected String getSuccessKey() {
+        return UnfollowTask.SUCCESS_KEY;
+    }
+
+    @Override
+    protected String getMessageKey() {
+        return UnfollowTask.MESSAGE_KEY;
+    }
+
+    @Override
+    protected String getExceptionKey() {
+        return UnfollowTask.EXCEPTION_KEY;
+    }
+
+    @Override
+    protected void handleSuccess(Message msg) {
+        observer.unFollowSuccess(true);
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(UnfollowTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(UnfollowTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.unFollowFailed("Failed to unfollow: " + message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.unFollowFailed("Failed to unfollow because of exception: " + exception.getMessage());
     }
 }
