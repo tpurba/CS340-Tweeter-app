5f1d909d1f1b157574dff8dc12b367ef4a720780
tpurba <tpurba@byu.edu>
2023-10-09 21:51:40 -0600

    Changed PostStatusHandler.java to implement HandlerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/PostStatusHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/PostStatusHandler.java
@@ -11,25 +11,58 @@ import edu.byu.cs.tweeter.client.model.service.backgroundTask.PostStatusTask;
 
 // PostStatusHandler
 //GETRIDOF
-public class PostStatusHandler extends Handler {
+public class PostStatusHandler extends HandlerTask<StatusService.MainActivityObserver> {
     private StatusService.MainActivityObserver observer;
 
     public PostStatusHandler(StatusService.MainActivityObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(), observer);
         this.observer = observer;
     }
 
     @Override
-    public void handleMessage(@NonNull Message msg) {
-        boolean success = msg.getData().getBoolean(PostStatusTask.SUCCESS_KEY);
-        if (success) {
-            observer.postSuccess("Successfully Posted!");
-        } else if (msg.getData().containsKey(PostStatusTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(PostStatusTask.MESSAGE_KEY);
-            observer.postFailed("Failed to post status: " + message);
-        } else if (msg.getData().containsKey(PostStatusTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(PostStatusTask.EXCEPTION_KEY);
-            observer.postFailed("Failed to post status because of exception: " + ex.getMessage());
-        }
+    protected String getSuccessKey() {
+        return PostStatusTask.SUCCESS_KEY;
+    }
+
+    @Override
+    protected String getMessageKey() {
+        return PostStatusTask.MESSAGE_KEY;
+    }
+
+    @Override
+    protected String getExceptionKey() {
+        return PostStatusTask.EXCEPTION_KEY;
+    }
+
+    @Override
+    protected void handleSuccess(Message msg) {
+        observer.postSuccess("Successfully Posted!");
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(PostStatusTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(PostStatusTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.postFailed("Failed to post status: " + message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.postFailed("Failed to post status because of exception: " + exception.getMessage());
     }
 }
