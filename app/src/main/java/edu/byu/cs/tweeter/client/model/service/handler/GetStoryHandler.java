c3fdaebb0f1ccb516434b284ca05cffb03c35f5b
tpurba <tpurba@byu.edu>
2023-10-09 21:18:07 -0600

    Changed GetStoryHandler.java to implement HandlerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetStoryHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetStoryHandler.java
@@ -12,27 +12,60 @@ import edu.byu.cs.tweeter.client.model.service.StoryService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetStoryTask;
 import edu.byu.cs.tweeter.model.domain.Status;
 
-public class GetStoryHandler extends Handler {
+public class GetStoryHandler extends HandlerTask<StoryService.StoryObserver>{
     private StoryService.StoryObserver observer;
 
     public GetStoryHandler(StoryService.StoryObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(), observer);
         this.observer = observer;
     }
 
     @Override
-    public void handleMessage(@NonNull Message msg) {
-        boolean success = msg.getData().getBoolean(GetStoryTask.SUCCESS_KEY);
-        if (success) {
-            List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetStoryTask.ITEMS_KEY);
-            boolean hasMorePages = msg.getData().getBoolean(GetStoryTask.MORE_PAGES_KEY);
-            observer.addMoreStory(statuses, hasMorePages);
-        } else if (msg.getData().containsKey(GetStoryTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(GetStoryTask.MESSAGE_KEY);
-            observer.displayError("Failed to get story: " + message);
-        } else if (msg.getData().containsKey(GetStoryTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(GetStoryTask.EXCEPTION_KEY);
-            observer.displayException(ex);
-        }
+    protected String getSuccessKey() {
+        return GetStoryTask.SUCCESS_KEY;
+    }
+
+    @Override
+    protected String getMessageKey() {
+        return GetStoryTask.MESSAGE_KEY;
+    }
+
+    @Override
+    protected String getExceptionKey() {
+        return GetStoryTask.EXCEPTION_KEY;
+    }
+
+    @Override
+    protected void handleSuccess(Message msg) {
+        List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetStoryTask.ITEMS_KEY);
+        boolean hasMorePages = msg.getData().getBoolean(GetStoryTask.MORE_PAGES_KEY);
+        observer.addMoreStory(statuses, hasMorePages);
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(GetStoryTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(GetStoryTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.displayError("Failed to get story: " + message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.displayException(exception);
     }
 }
