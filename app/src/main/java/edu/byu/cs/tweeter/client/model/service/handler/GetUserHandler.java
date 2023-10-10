092bae388c5a257af654482b9a6ea38942ba4f75
tpurba <tpurba@byu.edu>
2023-10-09 21:21:15 -0600

    Changed GetUserHandler.java to implement HandlerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetUserHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetUserHandler.java
@@ -10,27 +10,59 @@ import edu.byu.cs.tweeter.client.model.service.UserService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetUserTask;
 import edu.byu.cs.tweeter.model.domain.User;
 
-public class GetUserHandler extends Handler {//THIS THING HERE NEEDS TO GO
+public class GetUserHandler extends HandlerTask<UserService.GetUserObserver> {
     private UserService.GetUserObserver observer;
 
     public GetUserHandler(UserService.GetUserObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(), observer);
         this.observer = observer;
     }
 
     @Override
-    public void handleMessage(@NonNull Message msg) {
-        boolean success = msg.getData().getBoolean(GetUserTask.SUCCESS_KEY);
-        if (success) {
-            User user = (User) msg.getData().getSerializable(GetUserTask.USER_KEY);
-            observer.getUserSucceeded(user);
+    protected String getSuccessKey() {
+        return GetUserTask.SUCCESS_KEY;
+    }
 
-        } else if (msg.getData().containsKey(GetUserTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(GetUserTask.MESSAGE_KEY);
-            observer.getUserFailed("Failed to get user's profile:" + message);
-        } else if (msg.getData().containsKey(GetUserTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(GetUserTask.EXCEPTION_KEY);
-            observer.getUserFailed("Failed to get user's profile because of exception: " + ex.getMessage());
-        }
+    @Override
+    protected String getMessageKey() {
+        return GetUserTask.MESSAGE_KEY;
+    }
+
+    @Override
+    protected String getExceptionKey() {
+        return GetUserTask.EXCEPTION_KEY;
+    }
+
+    @Override
+    protected void handleSuccess(Message msg) {
+        User user = (User) msg.getData().getSerializable(GetUserTask.USER_KEY);
+        observer.getUserSucceeded(user);
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(GetUserTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(GetUserTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.getUserFailed("Failed to get user's profile:" + message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.getUserFailed("Failed to get user's profile because of exception: " + exception.getMessage());
     }
 }
