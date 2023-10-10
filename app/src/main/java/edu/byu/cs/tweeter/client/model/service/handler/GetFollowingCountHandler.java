6817ab69d23866498b938ce4b2de36abf801f8fb
tpurba <tpurba@byu.edu>
2023-10-09 21:11:40 -0600

    Changed GetFollowingCountHandler.java to implement HandlerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetFollowingCountHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/handler/GetFollowingCountHandler.java
@@ -9,26 +9,59 @@ import androidx.annotation.NonNull;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowingCountTask;
 
-public class GetFollowingCountHandler extends Handler {
+public class GetFollowingCountHandler extends HandlerTask<FollowService.MainActivityObserver> {
     private FollowService.MainActivityObserver observer;
 
     public GetFollowingCountHandler(FollowService.MainActivityObserver observer) {
-        super(Looper.getMainLooper());
+        super(Looper.getMainLooper(),observer);
         this.observer = observer;
     }
 
     @Override
-    public void handleMessage(@NonNull Message msg) {
-        boolean success = msg.getData().getBoolean(GetFollowingCountTask.SUCCESS_KEY);
-        if (success) {
-            int count = msg.getData().getInt(GetFollowingCountTask.COUNT_KEY);
-            observer.getFollowingCountSuccess(count);
-        } else if (msg.getData().containsKey(GetFollowingCountTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(GetFollowingCountTask.MESSAGE_KEY);
-            observer.getFollowingCountFailed("Failed to get following count: " + message);
-        } else if (msg.getData().containsKey(GetFollowingCountTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(GetFollowingCountTask.EXCEPTION_KEY);
-            observer.getFollowingCountFailed("Failed to get following count because of exception: " + ex.getMessage());
-        }
+    protected String getSuccessKey() {
+        return GetFollowingCountTask.SUCCESS_KEY;
+    }
+
+    @Override
+    protected String getMessageKey() {
+        return GetFollowingCountTask.MESSAGE_KEY;
+    }
+
+    @Override
+    protected String getExceptionKey() {
+        return GetFollowingCountTask.EXCEPTION_KEY;
+    }
+
+    @Override
+    protected void handleSuccess(Message msg) {
+        int count = msg.getData().getInt(GetFollowingCountTask.COUNT_KEY);
+        observer.getFollowingCountSuccess(count);
+    }
+
+    @Override
+    protected void createFailureMessage(Message msg) {
+        String message = msg.getData().getString(GetFollowingCountTask.MESSAGE_KEY);
+        handleFailure(message);
+    }
+
+    @Override
+    protected void createExceptionMessage(Message msg) {
+        Exception ex = (Exception) msg.getData().getSerializable(GetFollowingCountTask.EXCEPTION_KEY);
+        handleException(ex);
+    }
+
+    @Override
+    protected void doTask() {
+
+    }
+
+    @Override
+    public void handleFailure(String message) {
+        observer.getFollowingCountFailed("Failed to get following count: " + message);
+    }
+
+    @Override
+    public void handleException(Exception exception) {
+        observer.getFollowingCountFailed("Failed to get following count because of exception: " + exception.getMessage());
     }
 }
